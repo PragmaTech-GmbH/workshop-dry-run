c0f1cec9cf5542316fbbab30afba5c28cb7a9fc1
Philip Riecks <mail@philipriecks.de>
2026-06-19 14:43:02 +0200

    initial commit

---
+++ src/main/java/digital/pragmatech/workshop_dry_run/WorkshopDryRunApplication.java
@@ -0,0 +1,13 @@
+package digital.pragmatech.workshop_dry_run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopDryRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopDryRunApplication.class, args);
+	}
+
+}
